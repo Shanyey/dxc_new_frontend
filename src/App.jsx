17715394ac16ee8fee5d2e799ff3259a18af29b0
shayne <sleongxy@gmail.com>
2025-05-31 19:04:12 +0800

    Move login page files into a seperate folder Set wallpaper to only apply to loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./LoginPage.css";
+import DXCLogo from "../../assets/dxc-brand.png";
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Email: ${email} \n password: ${password}`); //TO BE REMOVED
+  };
+
+  return (
+    <div className="login-background">
+      <div className="wrapper">
+        <img src={DXCLogo} className="logo" />
+        <form onSubmit={handleSubmit}>
+          <h1>Login</h1>
+          <div className="input-box">
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(ev) => setEmail(ev.target.value)}
+            />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(ev) => setPassword(ev.target.value)}
+            />
+            <FaLock className="icon" />
+          </div>
+          <div className="forgot-password">
+            <a href="https://www.google.com/">Forgot password?</a>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
