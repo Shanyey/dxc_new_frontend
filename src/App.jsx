9884f215759b97ca703808f0764516a539bd1035
Dasuperhero <94967887+Dasuperhero@users.noreply.github.com>
2025-06-02 17:57:42 +0800

    handled merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import "./App.css";
-<<<<<<< HEAD
-=======
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ResetPage from "./pages/ResetPage/ResetPage";
->>>>>>> a87b7880a06c276745d3e2fee893d602576bf210
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import Home from "./pages/HomePage/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<Home />} />
-=======
         <Route path="/login" element={<LoginPage />} />
         <Route path="/reset" element={<ResetPage />} />
->>>>>>> a87b7880a06c276745d3e2fee893d602576bf210
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );
