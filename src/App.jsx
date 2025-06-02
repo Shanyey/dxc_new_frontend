8b1e4d24a0b73a202a1ba2f362b779ee0eb6dfd9
Dasuperhero <94967887+Dasuperhero@users.noreply.github.com>
2025-06-02 11:12:53 +0800

    added react router and edited browser image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
