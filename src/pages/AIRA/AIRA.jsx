21579bba943aab32cef814f7b06537565c436837
Dasuperhero <94967887+Dasuperhero@users.noreply.github.com>
2025-06-11 09:37:28 +0800

    removed aira.css from imports in aira.jsx

--- src/pages/AIRA/AIRA.jsx
+++ src/pages/AIRA/AIRA.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 // temporarily disable
 // import { auth } from "../Firebase";
-import "./AIRA.css";
 import DeepResearchInput from "../../components/AIRA/AIRA_InputQuery";
 import DeepInsightsDisplay from "../../components/AIRA/DeepInsightsDisplay";
 import DocumentsDisplay from "../../components/AIRA/DocumentsDisplay";
